b99748498cc70d174e95cd23ed504f184d03f8b0
AshakaE <ashakaegerega@gmail.com>
2021-03-18 09:46:01 +0100

    desktop ready

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
+    require('@tailwindcss/jit'),
     cssnano({
       preset: 'default',
     }),
